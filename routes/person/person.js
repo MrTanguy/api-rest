8a8f4bc27ea4ca9787937b35fa4fef796bfe58f3
MrTanguy <tanguy.meignier@gmail.com>
2023-10-22 11:13:51 +0200

    Implémentation de la fonction readOne

--- routes/person/person.js
+++ routes/person/person.js
@@ -3,19 +3,33 @@ const Joi = require('joi');
 const mysql = require('mysql');
 dotenv.config();
 
+
+const connection = () => {
+    return mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PWD
+    });
+}
+
 const person = Joi.object({
     firstname: Joi.string().max(255).required(),
     lastname: Joi.string().max(255).required(),
     birthdate: Joi.date().iso().required()
 })
 
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD
-});
+const convertDate = (date) => {
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+
+    return `${year}-${month}-${day}`
+}
+
 
 function createOne(request, response) {
+
+    const db = connection()
 
     // On vérifie la donnée
     const validator = person.validate(request.body) 
@@ -23,24 +37,24 @@ function createOne(request, response) {
     // On retourne des erreurs 
     if (validator.error) {
         if (validator.error.details[0].message.includes("birthdate")) {
-            response.send("Birthdate must follow this format : YYYY-MM-DD");
+            response.status(400).send("Birthdate must follow this format : YYYY-MM-DD");
         } else {
-            response.send(validator.error.details[0].message)
+            response.status(400).send(validator.error.details[0].message)
         }
     } else {
 
         // On envoie les données en BDD
         db.connect(function(err) {
             if (err) {
-                response.send('Something wrong happened !\n' + err);
+                response.status(400).send('Something wrong happened !\n' + err);
             }
             db.query(
                 `INSERT INTO \`api-rest\`.\`person\` (lastname, firstname, birthdate) VALUES ('${request.body.lastname}', '${request.body.firstname}', DATE('${request.body.birthdate}'));`, 
-                (err, result) => {
+                (err, _) => {
                     if (err) {
-                        response.send('Something wrong happened !\n' + err)
+                        response.status(400).send('Something wrong happened !\n' + err)
                     } else {
-                        response.send(`${request.body.lastname} ${request.body.firstname} created !`)
+                        response.status(201).send(`${request.body.lastname} ${request.body.firstname} created !`)
                     }
                 }
             );
@@ -49,8 +63,26 @@ function createOne(request, response) {
 }
 
 function readOne(request, response) {
-    // request.params.id
-    // ...
+
+    const db = connection()
+
+    db.connect(function(err) {
+        if (err) {
+            response.send('Something wrong happened !\n' + err);
+        }
+        db.query(
+            `SELECT id_person, lastname, firstname, birthdate FROM \`api-rest\`.\`person\` WHERE id_person = ${request.params.id}`,
+            (err, result) => {
+                db.end()
+                if (err) {
+                    return response.status(400).send('Something wrong happened !\n' + err)
+                } else {
+                    result[0]["birthdate"] = convertDate(result[0]["birthdate"])
+                    return response.status(200).send(result)
+                }
+            }
+        );
+    });
 }
 
 function readAll(request, response) {
